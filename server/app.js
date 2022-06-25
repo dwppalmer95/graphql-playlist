7f9d26f0d60bb6071dbc0df0a281a4e7b994f9d7
david palmer <dwppalmer95@gmail.com>
2022-06-25 16:21:24 -0400

    leftoff: 6/25 - implementing Apollog Server on the back end, setting up the infrastructure for Apollo Subscriptions. currently writing the resolvers

--- server/app.js
+++ server/app.js
@@ -2,6 +2,27 @@ const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 const cors = require('cors');
+const { ApolloServer, gql } = require('apollo-server');
+const books = require('../data/books');
+
+const typedefs = gql`
+  type Book {
+    id: ID
+    name: String
+    genre: String
+  }
+
+  type Query {
+    book: Book
+    allBooks: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: _.find(books, {id: args.id}),
+  },
+}
 
 const app = express();
 
