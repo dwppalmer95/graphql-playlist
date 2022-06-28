93bbada344a072c7605864bb0df7c667e442a4ff
david palmer <dwppalmer95@gmail.com>
2022-06-27 22:51:41 -0400

    started to implement websocket into app.ts. Switch app to ts language

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,14 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
-const schema = require('./schema/schema');
 const cors = require('cors');
 const { ApolloServer, gql } = require('apollo-server');
 const books = require('./data/books.js');
-const _ = require('lodash');
+import _ from 'lodash';
+import { createServer } from 'http';
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
 
 const typeDefs = gql`
   type Book {
@@ -24,13 +28,28 @@ const resolvers = {
     book: (parent, args) => _.find(books, {id: args.id}),
     allBooks: () => books,
   },
-}
+};
+
+const app = express();
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+const httpServer = createServer(app);
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: '/graphql',
+});
+const serverCleanup = useServer({ schema }, wsServer);
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
   csrfPrevention: true,
   cache: 'bounded',
+  plugins: [
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+    {
+      
+    }
+  ]
 });
 
 server.listen().then(({ url }) => console.log(`Server listening at ${url}`));
